ee39165dc510b88bee1bb2f80ad2ac3da1536e2e
ericnoharaleclair <eric.noharaleclair@gmail.com>
2024-11-21 16:01:28 -0500

    finished CRUD operations for alias

--- app/api/customURL/route.ts
+++ app/api/customURL/route.ts
@@ -1,33 +1,118 @@
 import getCollection, { URL_COLLECTION } from "@/db";
+import { get } from "http";
 import { NextResponse } from "next/server";
 
-export async function POST(req: Request) {
-  const { alias, url }: { alias: string; url: string } = await req.json();
-
-  const collection = await getCollection(URL_COLLECTION);
-
-  const result = await collection.findOne({ alias });
-
-  if (result) {
+export async function GET(req: Request) {
+  try {
+    const collection = await getCollection(URL_COLLECTION);
+    const results = await collection.find().toArray();
+    return NextResponse.json(results, { status: 200 });
+  } catch (err) {
+    console.error(err);
     return NextResponse.json(
-      { message: "Alias already exists" },
-      { status: 403 }
+      { message: "Error fetching documents" },
+      { status: 500 }
     );
   }
+}
 
-  const urlAlias = { alias: alias, url: url };
+export async function POST(req: Request) {
+  try {
+    const { alias, url }: { alias: string; url: string } = await req.json();
 
-  const response = await collection.insertOne(urlAlias);
+    const collection = await getCollection(URL_COLLECTION);
 
-  if (!response.acknowledged) {
+    const result = await collection.findOne({ alias });
+
+    if (result) {
+      return NextResponse.json(
+        { message: "Alias already exists" },
+        { status: 403 }
+      );
+    }
+
+    const urlAlias = { alias: alias, url: url };
+
+    const response = await collection.insertOne(urlAlias);
+
+    if (!response.acknowledged) {
+      return NextResponse.json(
+        { message: "Error adding alias" },
+        { status: 400 }
+      );
+    }
+
+    return NextResponse.json(
+      { message: "Successfully saved alias" },
+      { status: 200 }
+    );
+  } catch (err) {
+    console.error(err);
     return NextResponse.json(
       { message: "Error adding alias" },
-      { status: 400 }
+      { status: 500 }
     );
   }
+}
 
-  return NextResponse.json(
-    { message: "Successfully saved alias" },
-    { status: 200 }
-  );
+export async function PUT(req: Request) {
+  try {
+    const { alias, url, aliasNew, urlNew } = await req.json();
+
+    if (!alias || !url || !aliasNew || !urlNew) {
+      return NextResponse.json(
+        { message: "Missing required fields" },
+        { status: 400 }
+      );
+    }
+
+    const filter = { alias: alias, url: url };
+    const update = { $set: { alias: aliasNew, url: urlNew } };
+
+    const collection = await getCollection(URL_COLLECTION);
+
+    const result = await collection.updateOne(filter, update);
+
+    if (result.matchedCount === 0) {
+      return NextResponse.json({ message: "Alias not found" }, { status: 404 });
+    }
+
+    return NextResponse.json(
+      { message: "Alias updated successfully" },
+      { status: 200 }
+    );
+  } catch (err) {
+    console.error(err);
+    return NextResponse.json(
+      { message: "Error updating alias" },
+      { status: 500 }
+    );
+  }
+}
+
+export async function DELETE(req: Request) {
+  try {
+    const { alias, url } = await req.json();
+
+    const collection = await getCollection(URL_COLLECTION);
+
+    const filter = { alias: alias, url: url };
+
+    const result = await collection.deleteOne(filter);
+
+    if (result.deletedCount === 0) {
+      return NextResponse.json({ message: "Alias not found" }, { status: 404 });
+    }
+
+    return NextResponse.json(
+      { message: "Successfully deleted alias" },
+      { status: 200 }
+    );
+  } catch (err) {
+    console.error(err);
+    return NextResponse.json(
+      { message: "Error deleting alias" },
+      { status: 500 }
+    );
+  }
 }
